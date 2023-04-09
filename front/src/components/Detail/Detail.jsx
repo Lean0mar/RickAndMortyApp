982f4f35e79805b110fd1c7f10ef7c38b87fdd4c
lean0mar <lm.martinez.leandro@gmail.com>
2023-04-08 22:25:14 -0300

    RickAndMortyApp

--- front/src/components/Detail/Detail.jsx
+++ front/src/components/Detail/Detail.jsx
@@ -1,19 +1,23 @@
 import useCharacter from "../../hooks/useCharacter";
 import styles from "./Detail.module.css"
+import { Link } from "react-router-dom";
 
 const Detail = () => {
   const character = useCharacter();
 
   return (
     <div className={styles.conteiner}>
+
       {character.name ? (
         <div className={styles.info}>
+      <Link className={styles.button} to="/home">Home</Link>          
+          <img src={character.image} alt="img" />
           <h2>{character.name}</h2>
           <p>{character.status}</p>
           <p>{character.species}</p>
           <p>{character.gender}</p>
           <p>{character.origin?.name}</p>
-          <img src={character.image} alt="img" />
+
         </div>
       ) : (
         <h3>Loading...</h3>
